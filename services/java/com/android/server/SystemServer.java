ef46892cc90f1d891d5e73bcb5c490479bb489a8
klozz <carlosj@klozz.dev>
2023-10-02 11:20:50 +0000

    Revert "base: Implement Lineage health service"
    This reverts commit 350647c45b24b0f9bc6e274e80ac882f1a0dfa1b.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -221,7 +221,6 @@ import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 import com.android.server.derp.LineageGlobalActionsService;
 import com.android.server.derp.display.LiveDisplayService;
-import com.android.server.derp.health.HealthInterfaceService;
 import com.android.server.derp.ParallelSpaceManagerService;
 
 import dalvik.system.VMRuntime;
@@ -2581,9 +2580,6 @@ public final class SystemServer implements Dumpable {
                 t.traceEnd();
                 t.traceBegin("StartLiveDisplayService");
                 mSystemServiceManager.startService(LiveDisplayService.class);
-                t.traceEnd();
-                t.traceBegin("StartHealthService");
-                mSystemServiceManager.startService(HealthInterfaceService.class);
                 t.traceEnd();
             }
         }
